7c0c6e981052959e606369749cc5d08985df710f
JB <jb@codecorsair.com>
2016-01-26 20:12:04 -0500

    fix fetch

--- src/restapi/RestAPI.ts
+++ src/restapi/RestAPI.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import {fetch} from 'isomorphic-fetch';
+import 'isomorphic-fetch';
 import {Promise} from 'es6-promise';
 
 import CoreSettings from '../core/CoreSettings';
