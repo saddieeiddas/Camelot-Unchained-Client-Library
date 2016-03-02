3622939df928e824c4ccb37e9cf85662da9ff659
Brian Green <bgreen@citystateentertainment.com>
2016-03-02 17:22:13 -0500

    New mapping from server to ui parts.

--- src/components/woundframe/components/Doll.tsx
+++ src/components/woundframe/components/Doll.tsx
@@ -35,9 +35,9 @@ function getState(damage: number, max: number): number {
 function getPart(clientId: number): number {
   switch (clientId) {
     case 0: return 3;
-    case 1: return 1;
-    case 2: return 2;
-    case 3: return 0;
+    case 1: return 0;
+    case 2: return 1;
+    case 3: return 2;
     case 4: return 4;
     case 5: return 5;
   }
